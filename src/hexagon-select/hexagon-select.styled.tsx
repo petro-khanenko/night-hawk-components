dfbcba898323d3b40ee9c1a301e8deec1ab7528c
Petro Khanenko <75987847+petro-khanenko@users.noreply.github.com>
2022-10-20 11:06:00 +0300

    Add select

---
+++ src/hexagon-select/hexagon-select.styled.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Grid, Theme } from "@mui/material";
+import Select from "@mui/material/Select";
+import { TiArrowSortedDown } from "react-icons/ti";
+import BigBorder from "../../../../assets/icons/input-big-border.svg";
+import BigBorderError from "../../../../assets/icons/input-big-border-error.svg";
+import { COLORS } from "../../../theme";
+
+const ArrowIcon: React.FC<any> = React.forwardRef((props, ref) => (
+	<TiArrowSortedDown
+		size={24}
+		color={COLORS.deepViolet}
+		style={{ marginRight: '9px' }}
+		{...props}
+		ref={ref}
+	/>
+));
+
+const StandardSelect = React.forwardRef((props, ref) => (
+	<Select variant="standard" disableUnderline IconComponent={ArrowIcon} {...props} ref={ref} />
+));
+
+export const SelectContainer = styled(Grid)<{ disabled?: boolean | undefined; error?: boolean; theme?: Theme }>`
+	position: relative;
+	background-repeat: no-repeat;
+	min-height: 50px;
+	margin-bottom: ${({theme}) => theme.spacing(1)};
+	background-image: ${(props) => (props.error ? `url(${BigBorderError})` : `url(${BigBorder})`)};
+	min-width: 420px;
+`;
+
+export const SelectField = styled(StandardSelect)<{theme?: Theme}>`
+	background-color: transparent;
+	min-width: 395px;
+	align-items: center;
+	margin-left: ${({theme}) => theme.spacing(1.5)};
+	cursor: pointer;
+	& .MuiSelect-select {
+		padding-left: 15px;
+		margin-top: 11px;
+		min-height: 28px !important;
+		align-items: center;
+	}
+`;
+
+export const Styled = {
+	SelectContainer,
+	Select,
+	Option,
+	SelectField,
+};
---
+++ src/hexagon-select/index.tsx
@@ -0,0 +1,49 @@
+import React, {ReactNode} from "react";
+import {Styled} from "./hexagon-select.styled";
+import {Grid, Theme} from "@mui/material";
+import * as GlobalTypography from "../global-typography";
+
+export interface IFormSelectProps {
+	name: string;
+	label?: string;
+	disabled?: boolean | undefined;
+	isErrorExists?: boolean | undefined;
+	subLabel?: string;
+	theme?: Theme;
+	children: ReactNode;
+}
+
+export const FormSelect: React.FC<IFormSelectProps> = ({
+	disabled,
+	isErrorExists,
+	label,
+	subLabel,
+	theme,
+	children
+}) => {
+
+	return (
+		<Grid container direction='column' justifyContent='center' alignItems='center'>
+			<Grid container alignItems='center' justifyContent='space-between' pl='1.5rem' pr='2.5rem'>
+				{
+					label && (
+						<GlobalTypography.Text variant="body2" colorVariant={theme?.palette.secondary.main} fontWeight='fontWeightMedium'>
+							{label}
+						</GlobalTypography.Text>
+					)
+				}
+				{
+					subLabel && (
+						<GlobalTypography.Text variant="body2" colorVariant={theme?.palette.secondary.main} pl='1rem'>
+							{subLabel}
+						</GlobalTypography.Text>
+					)
+				}
+			</Grid>
+
+			<Styled.SelectContainer error={isErrorExists} disabled={disabled}>
+				{ children }
+			</Styled.SelectContainer>
+		</Grid>
+	);
+};
