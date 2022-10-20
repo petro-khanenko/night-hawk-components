e72fef5d8bf15cd48a0a25772ae460c7fa3afe79
Petro Khanenko <75987847+petro-khanenko@users.noreply.github.com>
2022-10-20 11:49:23 +0300

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,16 +25,16 @@ module.exports = {
                 test: /\.(ts|tsx)?$/,
                 use: ['ts-loader'],
             },
-            {oneOf: [
-                    {
-                        test: /\.svg$/,
-                        type: "asset/inline",
-                    },
+            // {oneOf: [
+            //         {
+            //             test: /\.svg$/,
+            //             type: "asset/inline",
+            //         },
                     {
                         test: /\.(jpg|png|svg)$/,
                         type: "asset/resource",
                     },
-                ]},
+                // ]},
         ],
     }
 }
